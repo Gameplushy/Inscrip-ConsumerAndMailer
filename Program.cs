1e79f50358631de3149dd3d1063c9dd6cfa26598
Gameplushy <victor_f@orange.fr>
2023-11-24 09:34:32 +0100

    RabbitMQ queue reading

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Press enter to finish the program.");
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672/%2f");
+
+using (var conn = factory.CreateConnection())
+{
+    using (var ch = conn.CreateModel())
+    {
+        var consumer = new EventingBasicConsumer(ch);
+
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine($"Received message: {message}");
+        };
+
+        ch.BasicConsume(queue: "myQueue", autoAck: true, consumer: consumer);
+    }
+}
+
+Console.ReadLine(); //This exists so the program doesn't end immediately
